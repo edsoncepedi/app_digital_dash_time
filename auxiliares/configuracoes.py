d0b0848dcdbc9aacfd7bb0746e4d3305bb3d86d1
edsoncepedi <edson.alves@cepedi.org.br>
2025-11-11 11:49:57 -0300

    Update configuracoes.py

--- auxiliares/configuracoes.py
+++ auxiliares/configuracoes.py
@@ -9,4 +9,5 @@ ultimo_posto_bios = int(os.getenv('NUMERO_POSTOS', 2))
 
 cartao_palete = {" C3 C3 64 AD": "PLT01", 
                  " 39 7E E8 97": "PLT02",
-                 " 53 4A 68 AC": "PLT03"}
+                 " 53 4A 68 AC": "PLT03",
+                 " 03 48 4E AC": "PLT04"}
